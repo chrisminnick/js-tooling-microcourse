d25f8b8c54c70ce81c060c2f55e700f101fd12f5
Chris Minnick <chris@minnick.com>
2021-10-28 02:15:12 -0700

    added solutions for 9 and 10

---
+++ solutions/lab10/src/scripts/app.js
@@ -0,0 +1,7 @@
+var sayHello = require('./sayHello.js');
+window.addEventListener('load', function(){
+    document.getElementById('welcome-message')
+        .innerHTML = sayHello.greet('Your name');
+});
+
+console.log('Hello, World!');
